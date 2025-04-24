0395ae2c8b0c96f1f9199418e38ef17739495be5
Jhon Noises <jhonseagle@gmail.com>
2025-04-24 16:11:52 -0500

    Se crean las interfaces complementarias de Pokemon, se actualiza el componente Pokemon y el servicio pokemon.service

--- PokeQuest/src/app/components/pokemon/pokemon.component.ts
+++ PokeQuest/src/app/components/pokemon/pokemon.component.ts
@@ -5,6 +5,11 @@ import { Subject, takeUntil } from 'rxjs';
 import { IPokemon } from '../../interfaces/pokemon.interface';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { IAbilitiy } from '../../interfaces/ability.interface';
+import { IMove } from '../../interfaces/move.interface';
+import { IEvolutionChain } from '../../interfaces/evolution_chain.interface';
+import { ISpecie } from '../../interfaces/specie.interface';
+import { IChain } from '../../interfaces/chain.interface';
 
 @Component({
   selector: 'app-pokemon',
@@ -59,7 +64,48 @@ export class PokemonComponent {
     types: [],
     weight: 0
   }
+  public ability: IAbilitiy = {
+      id: 0,
+      name: '',
+      is_main_series: false,
+      generation: {
+        name: '',
+        url: ''
+      },
+      names: [],
+      effect_entries: [],
+      effect_changes: [],
+      flavor_text_entries: [],
+      pokemon: [],
+  }
 
+  public move: IMove = {
+    accuracy: 0,
+    //contest_combos: { normal: { use_after: INameAndUrl[], use_before: INameAndUrl[]}, super: { use_after: INameAndUrl[], use_before: INameAndUrl[]}}
+    id: 0,
+    name: "",
+    names: []
+  }
+
+  public specie: ISpecie = {
+    id: 0,
+    name: "",
+    evolution_chain: { url: "" }
+  }
+  public evolutionChain: IEvolutionChain = {
+    id:0,
+    chain: {
+      evolves_to: [],
+      species: {
+        name: '',
+        url: ''
+      }
+    }
+  }
+  public cadenaTipos: string[] = [];
+  public cadenaHabilidades: string[] = [];
+  public cadenaMovimientos: string[] = [];
+  public cadenaEvoluciones: string[] = [];
   CargarDetalles(pokemon: string){
     console.log(pokemon)
     this.PokemonService.searchByName(pokemon)
@@ -67,10 +113,110 @@ export class PokemonComponent {
       .subscribe(
           res => {
             this.pokemon = res;
-              },
+            let tipos = "";
+            for(let j = 0; j<this.pokemon.types.length; j++){
+              tipos += this.pokemon.types[j].type.name + " "
+            }
+            for(let j = 0; j<this.pokemon.abilities.length; j++){
+              this.cadenaHabilidades.push(this.pokemon.abilities[j].ability.name);
+            }
+            for(let j = 0; j<this.pokemon.moves.length; j++){
+              this.cadenaMovimientos.push(this.pokemon.moves[j].move.name);
+            }
+            tipos = tipos.trimEnd();
+            tipos = tipos.replace("normal", "Normal");
+            tipos = tipos.replace("fighting", "Lucha");
+            tipos = tipos.replace("flying", "Volador");
+            tipos = tipos.replace("poison", "Veneno");
+            tipos = tipos.replace("ground", "Tierra");
+            tipos = tipos.replace("rock", "Roca");
+            tipos = tipos.replace("bug", "Bicho");
+            tipos = tipos.replace("ghost", "Fantasma");
+            tipos = tipos.replace("steel", "Acero");
+            tipos = tipos.replace("fire", "Fuego");
+            tipos = tipos.replace("water", "Agua");
+            tipos = tipos.replace("grass", "Planta");
+            tipos = tipos.replace("electric", "Eléctrico");
+            tipos = tipos.replace("psychic", "Psíquico");
+            tipos = tipos.replace("ice", "Hielo");
+            tipos = tipos.replace("dragon", "Dragón");
+            tipos = tipos.replace("dark", "Siniestro");
+            tipos = tipos.replace("fairy", "Hada");
+            this.cadenaTipos = tipos.split(" ");
+            for(let i = 0; i < this.cadenaHabilidades.length; i++){
+              this.PokemonService.searchAbilityByName(this.cadenaHabilidades[i])
+              .pipe(takeUntil(this.ngUnsubscribe))
+              .subscribe(
+                  res => {
+                    this.ability = res;
+                    let habilidad = "";
+                    for(let j = 0; j<this.ability.names.length; j++){
+                      if(this.ability.names[j].language.name == "es"){
+                        habilidad = this.ability.names[j].name
+                      }
+                    }
+                    this.cadenaHabilidades[i] = habilidad;
+                    console.log(habilidad)
+                  },
+                  error => {
+                      console.log(error);
+                  }
+              );
+            }
+            for(let i = 0; i < this.cadenaMovimientos.length; i++){
+              this.PokemonService.searchMoveByName(this.cadenaMovimientos[i])
+              .pipe(takeUntil(this.ngUnsubscribe))
+              .subscribe(
+                  res => {
+                    this.move = res;
+                    let movimiento = "";
+                    for(let j = 0; j<this.move.names.length; j++){
+                      if(this.move.names[j].language.name == "es"){
+                        movimiento = this.move.names[j].name
+                      }
+                    }
+                    this.cadenaMovimientos[i] = movimiento;
+                    console.log(movimiento)
+                  },
+                  error => {
+                      console.log(error);
+                  }
+              );
+            }
+            this.PokemonService.searchSpecieByName(this.pokemon.species.name)
+              .pipe(takeUntil(this.ngUnsubscribe))
+              .subscribe(
+                  res => {
+                    this.specie = res;
+                    this.PokemonService.searchEvolutionChainByURL(this.specie.evolution_chain.url)
+                    .pipe(takeUntil(this.ngUnsubscribe))
+                    .subscribe(
+                        res => {
+                          this.evolutionChain = res;
+                          this.cadenaEvoluciones.push(this.evolutionChain.chain.species.name);
+                          this.ObtenerEvoluciones(this.evolutionChain.chain.evolves_to);
+                        },
+                        error => {
+                            console.log(error);
+                        }
+                    );
+                  },
+                  error => {
+                      console.log(error);
+                  }
+              );
+          },
           error => {
               console.log(error);
           }
       );
+      
+  }
+  ObtenerEvoluciones(cadenaEvolutiva: IChain[]){
+    if(cadenaEvolutiva.length == 0){
+      return
+    }
+    this.cadenaEvoluciones.push(cadenaEvolutiva[0].species.name)
+    this.ObtenerEvoluciones(cadenaEvolutiva[0].evolves_to)
   }
 }
